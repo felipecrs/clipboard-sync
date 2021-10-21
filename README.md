68ffbf5b103282706fe61dc3f9ed02d1fa12f787
Felipe Santos <felipecassiors@gmail.com>
2021-10-21 02:53:24 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Clipboard Sync
+
+![Icon](./res/clipboard.png)
+
+Icon made by [Freepik](https://www.flaticon.com/authors/freepik) and [Flat Icons](https://www.flaticon.com/authors/flat-icons) from [www.flaticon.com](https://www.flaticon.com/).
--- src/index.js
+++ src/index.js
@@ -1,46 +1,291 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, Tray, Menu, dialog, shell } = require("electron");
+const path = require("path");
+const Store = require("electron-store");
+const clipboardListener = require("clipboard-event");
+const clipboard = require("clipboardy");
+const chokidar = require("chokidar");
+const fs = require("fs");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+let appIcon = null;
+let store = new Store();
+
+let syncFolder = null;
+
+let lastTextWritten = "";
+let lastTimeWritten = "";
+
+let lastTextRead = "";
+let lastTimeRead = "";
+
+let clipboardFilesWatcher = null;
+let excludeFilesWatcher = null;
+
+let iconWaiter = null;
+
+const setIconFor5Seconds = (icon) => {
+  appIcon.setImage(path.resolve(__dirname, `../res/${icon}.png`));
+
+  if (iconWaiter) {
+    clearTimeout(iconWaiter);
+  }
+  iconWaiter = setTimeout(() => {
+    appIcon.setImage(path.resolve(__dirname, "../res/clipboard.png"));
+  }, 5000);
+};
+
+// Writes clipboard to file
+const writeClipboardToFile = () => {
+  let textToWrite = "";
+  try {
+    textToWrite = clipboard.readSync();
+  } catch (error) {
+    console.error("Error reading current clipboard");
+    return;
+  }
+  if (
+    !textToWrite ||
+    lastTextRead === textToWrite ||
+    lastTextWritten === textToWrite
+  ) {
+    return;
+  }
+
+  const writeTime = `${Date.now()}`;
+  const filePath = path.join(syncFolder, `${writeTime}.txt`);
+
+  lastTimeWritten = writeTime;
+  lastTextWritten = textToWrite;
+
+  console.log(`Writing clipboard to ${filePath}`);
+  fs.writeFileSync(filePath, textToWrite, {
+    encoding: "utf8",
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  setIconFor5Seconds("clipboard_sent");
+};
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+const readClipboardFromFile = (filePath) => {
+  const fileName = path.parse(filePath).name;
+
+  if (!new Date(parseInt("1243123123")).getTime() > 0) {
+    return;
+  }
+
+  let currentText = "";
+  try {
+    currentText = clipboard.readSync();
+  } catch (error) {
+    console.error("Error reading current clipboard");
+    return;
+  }
+
+  let newText = "";
+  try {
+    newText = fs.readFileSync(filePath, {
+      encoding: "utf8",
+    });
+  } catch (error) {
+    console.error(`Error reading text from file ${fileName}`);
+    return;
+  }
+
+  // Prevents writing duplicated text to clipboard
+  if (!newText || currentText === newText) {
+    return;
+  }
+
+  const currentFileTime = fileName;
+  // Skips the read if a newer file was already wrote
+  if (lastTimeWritten && currentFileTime <= lastTimeWritten) {
+    return;
+  }
+
+  // Skips if a newer file was already read
+  if (lastTimeRead && currentFileTime <= lastTimeRead) {
+    return;
+  }
+
+  lastTextRead = newText;
+  lastTimeRead = currentFileTime;
+
+  console.log(`Reading clipboard from ${filePath}`);
+  clipboard.writeSync(newText);
+
+  setIconFor5Seconds("clipboard_received");
+};
+
+const askForFolder = () => {
+  const previousFolder = store.get("folder");
+
+  let folderSelected = dialog.showOpenDialogSync({
+    title: "Select folder to save and read clipboard files",
+    properties: ["openDirectory"],
+    defaultPath: previousFolder,
+  });
+
+  if (folderSelected) {
+    folderSelected = folderSelected[0];
+  }
+
+  if (!folderSelected && !previousFolder) {
+    dialog.showErrorBox(
+      "Folder was not selected",
+      "Please start the application again to select a folder."
+    );
+    finish(1);
+    return;
+  } else if (!folderSelected) {
+    return;
+  }
+  syncFolder = folderSelected;
+  store.set("folder", folderSelected);
+
+  if (folderSelected !== previousFolder) {
+    reload();
+  }
+};
+
+const initialize = () => {
+  syncFolder = store.get("folder");
+
+  if (
+    !syncFolder ||
+    (fs.existsSync(syncFolder) && !fs.lstatSync(syncFolder).isDirectory())
+  ) {
+    askForFolder();
+  }
+
+  if (!fs.existsSync(syncFolder)) {
+    fs.mkdirSync(syncFolder);
+  }
+
+  if (store.get("send", true)) {
+    clipboardListener.startListening();
+
+    clipboardListener.on("change", writeClipboardToFile);
+  }
+
+  if (store.get("receive", true)) {
+    // Watches for files and reads clipboard from it
+    clipboardFilesWatcher = chokidar
+      .watch(`${syncFolder}/*.txt`, {
+        ignoreInitial: true,
+        depth: 1,
+      })
+      .on("add", readClipboardFromFile);
+  }
+
+  if (store.get("autoCleanup", true)) {
+    // Remove files older than 5 minutes
+    excludeFilesWatcher = chokidar
+      .watch(`${syncFolder}/*.txt`, {
+        depth: 1,
+      })
+      .on("add", (filePath) => {
+        const currentTimeMinus5Min = `${Date.now() - 300000}`;
+        if (path.parse(filePath).name <= currentTimeMinus5Min) {
+          fs.unlinkSync(filePath);
+        }
+      });
+  }
+};
+
+const cleanup = () => {
+  try {
+    clipboardListener.stopListening();
+  } catch (error) {
+    console.error("Error stopping clipboard listener");
+  }
+
+  if (clipboardFilesWatcher) {
+    clipboardFilesWatcher.close();
+  }
+
+  if (excludeFilesWatcher) {
+    excludeFilesWatcher.close();
+  }
+};
+
+const reload = () => {
+  console.log("Reloading configuration...");
+  cleanup();
+  initialize();
+};
+
+const finish = (exitCode = 0) => {
+  cleanup();
+  app.exit(exitCode);
+};
+
+const handleSendCheckBox = (checkBox) => {
+  store.set("send", checkBox.checked);
+  reload();
+};
+
+const handleReceiveCheckBox = (checkBox) => {
+  store.set("receive", checkBox.checked);
+  reload();
+};
+
+const handleCleanupCheckBox = (checkBox) => {
+  store.set("autoCleanup", checkBox.checked);
+  reload();
+};
+
+const createAppIcon = () => {
+  appIcon = new Tray(path.resolve(__dirname, "../res/clipboard.png"));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Send",
+      type: "checkbox",
+      checked: store.get("send", true),
+      click: handleSendCheckBox,
+    },
+    {
+      label: "Receive",
+      type: "checkbox",
+      checked: store.get("receive", true),
+      click: handleReceiveCheckBox,
+    },
+    {
+      label: "Auto-clean",
+      type: "checkbox",
+      checked: store.get("autoCleanup", true),
+      click: handleCleanupCheckBox,
+    },
+    { type: "separator" },
+    { label: "Change folder", type: "normal", click: askForFolder },
+    {
+      label: "Open folder",
+      type: "normal",
+      click: () => {
+        shell.openPath(syncFolder);
+      },
+    },
+    { type: "separator" },
+    { label: "Exit", type: "normal", click: finish },
+  ]);
+  appIcon.setToolTip("Clipboard Sync");
+  appIcon.setContextMenu(contextMenu);
+
+  initialize();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createAppIcon);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on("window-all-closed", () => {
+  finish();
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+app.on("before-quit", () => {
+  cleanup();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
