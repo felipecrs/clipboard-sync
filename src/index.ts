b6a46a773f096197b39f109120cf797bcfae389e
Felipe Santos <felipecassiors@gmail.com>
2021-11-29 23:50:23 -0300

    Open general releases page on new version

--- src/index.ts
+++ src/index.ts
@@ -636,7 +636,9 @@ const checkForUpdatesPress = async () => {
         `https://github.com/felipecrs/clipboard-sync/releases/download/v${update.newVersion}/Clipboard.Sync-${update.newVersion}.Setup.exe`
       );
     }
-    shell.openExternal(update.newVersionUrl);
+    shell.openExternal(
+      "https://github.com/felipecrs/clipboard-sync/releases/latest"
+    );
   } else {
     new Notification({
       title: "No updates found",
