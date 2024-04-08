7137d2f9403c88bc0687666f044002e8275da53f
Felipe Santos <felipecassiors@gmail.com>
2024-04-08 20:30:11 +0100

    improve prepare

--- tools/prepare.js
+++ tools/prepare.js
@@ -1,6 +1,8 @@
 import { chmodSync } from "node:fs";
 
-for (const platform of ["mac", "linux"]) {
+// https://github.com/sudhakar3697/node-clipboard-event/blob/0879a167f5643908349ff6b70a9365f9acdb652e/README.md#usage
+const platform = process.platform === "darwin" ? "mac" : process.platform;
+if (platform === "mac" || platform === "linux") {
   chmodSync(
     `./node_modules/clipboard-event/platform/clipboard-event-handler-${platform}`,
     0o755
