d501495c2719f3aff96801c0992da1c0eb945e98
Felipe Santos <felipecassiors@gmail.com>
2024-04-09 12:54:14 -0300

    Add Flatpak for Linux

--- forge.config.js
+++ forge.config.js
@@ -4,43 +4,85 @@ import path from "node:path";
 
 import { MakerDMG } from "@electron-forge/maker-dmg";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerFlatpak } from "@electron-forge/maker-flatpak";
 
-const getAppIcon = () => {
-  const iconExtension =
-    process.platform === "win32"
-      ? "ico"
-      : process.platform === "darwin"
-        ? "icns"
-        : "png";
-
-  return path.resolve(
-    import.meta.dirname,
-    `./resources/appicons/${iconExtension}/icon.${iconExtension}`,
-  );
-};
+const iconExtension =
+  process.platform === "win32"
+    ? "ico"
+    : process.platform === "darwin"
+      ? "icns"
+      : "png";
+const iconRelative = `resources/appicons/${iconExtension}/icon.${iconExtension}`;
+const icon = path.join(import.meta.dirname, iconRelative);
 
 /** @type {import("@electron-forge/shared-types").ForgeConfig} */
 const config = {
   packagerConfig: {
-    icon: getAppIcon(),
+    icon,
     ignore: [
       /^\/(src)|(tools)|(.github)|(.vscode)/,
-      /\/(.eslintrc.json)|(.gitignore)|(.gitattributes)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.json)|(bindl.config.js)|(bindl.config.js)|(README.md)$/,
+      /\/(.gitignore)|(.gitattributes)|(electron.vite.config.ts)|(eslint.config.js)|(forge.config.js)|(prettier.config.js)|(renovate.json)|(tsconfig.json)|(README.md)$/,
     ],
     // Prevents the app from showing up in the dock on macOS
     extendInfo: {
       LSUIElement: true,
     },
+    // https://github.com/electron/forge/issues/2805#issuecomment-3193871995
+    ...(process.platform === "linux"
+      ? { executableName: "clipboard-sync" }
+      : {}),
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({
       name: "clipboard_sync",
-      setupIcon: getAppIcon(),
-      iconUrl: getAppIcon(),
+      setupIcon: icon,
+      iconUrl: `https://raw.githubusercontent.com/felipecrs/clipboard-sync/refs/heads/master/${iconRelative}`,
     }),
     new MakerDMG({
-      icon: getAppIcon(),
+      icon,
+    }),
+    new MakerFlatpak({
+      options: {
+        id: "io.github.felipecrs.ClipboardSync",
+        // @ts-expect-error - this is the correct way
+        icon: {
+          "512x512": icon,
+        },
+        categories: ["Utility"],
+        sdk: "org.freedesktop.Sdk",
+        runtime: "org.freedesktop.Platform",
+        runtimeVersion: "24.08",
+        base: "org.electronjs.Electron2.BaseApp",
+        baseVersion: "24.08",
+        finishArgs: [
+          // Needed to monitor clipboard
+          "--socket=system-bus",
+          // Default permissions
+          "--socket=x11",
+          "--share=ipc",
+          "--device=dri",
+          "--socket=pulseaudio",
+          "--filesystem=home",
+          "--env=TMPDIR=/var/tmp",
+          "--share=network",
+          "--talk-name=org.freedesktop.Notifications",
+        ],
+        // https://github.com/electron/forge/issues/2805
+        modules: [
+          {
+            name: "zypak",
+            sources: [
+              {
+                type: "git",
+                url: "https://github.com/refi64/zypak",
+                // https://github.com/refi64/zypak/releases
+                tag: "v2024.01.17",
+              },
+            ],
+          },
+        ],
+      },
     }),
   ],
 };
