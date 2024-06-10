76d38f6e278fd2bbed034d139365ab25c2014bf8
Felipe Santos <felipecassiors@gmail.com>
2024-06-10 20:23:33 -0300

    Remove unnecessary import process

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+// This file exists so that the Prettier extension for VS Code uses Prettier
+// from the project's dependencies instead of the one bundled with the extension.
+// That's why the configuration is empty.
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Options}
+ * */
+const config = {};
+
+export default config;
--- tools/prepare.js
+++ tools/prepare.js
@@ -1,7 +1,6 @@
 // @ts-check
 
 import { chmodSync } from "node:fs";
-import process from "node:process";
 
 // https://github.com/sudhakar3697/node-clipboard-event/blob/0879a167f5643908349ff6b70a9365f9acdb652e/README.md#usage
 const platform = process.platform === "darwin" ? "mac" : process.platform;
